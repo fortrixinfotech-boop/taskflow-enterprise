40be67bd9795ab10d1da93f0e3c31d2aa8c6c3f2
Fortrix Infotech <fortrixinfotech@gmail.com>
2026-01-28 12:48:33 +0530

    feat: add auth request and response dtos (#6)

---
+++ backend/TaskFlow.Api/Models/Auth/AuthResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskFlow.Api.Models.Auth
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
---
+++ backend/TaskFlow.Api/Models/Auth/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskFlow.Api.Models.Auth
+{
+    public class LoginRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
