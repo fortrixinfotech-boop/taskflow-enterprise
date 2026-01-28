282edb7320813dddef35a6fe4e5c5b91f2ec950d
Fortrix <fortrixinfotech@gmail.com>
2026-01-28 12:47:19 +0530

    feat: add auth request and response dtos

---
+++ backend/TaskFlow.Api/Models/Auth/RegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskFlow.Api.Models.Auth
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public string FullName { get; set; } = null!;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required, MinLength(6)]
+        public string Password { get; set; } = null!;
+    }
+}
